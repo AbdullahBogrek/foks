30eb5b0c5b90035089979ec229f5c647f31431ef
Abdullah Böğrek <49994631+AbdullahBogrek@users.noreply.github.com>
2021-04-20 19:07:32 +0300

    Discord Bot
    Discord API with python.

---
+++ Discord/bot.py
@@ -0,0 +1,100 @@
+import os
+import discord
+from discord.ext import commands    
+from dotenv import load_dotenv
+from forex_python.converter import CurrencyRates
+
+# .env file 
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Bot
+client = commands.Bot(command_prefix = "!")
+
+# Currency
+c = CurrencyRates()
+
+# Current Currency with forex_python.conventer
+def current(x: str, y: str):
+    return c.get_rate(x, y)
+
+# Convert Currency with forex_python.conventer 
+def convert(x: str, y: str, z: float):
+    converted = c.convert(x, y, z)
+    return converted
+
+def rando(x: int, y: int):
+    return random.randint(x, y)
+
+f = open("/home/abdullah/Desktop/Discord/Discord/files/words.txt","r")
+words = f.read()
+
+@client.event
+async def on_ready():
+    print(f"{client.user.name} is online...")
+
+@client.event
+async def on_member_join(member):
+    channel = discord.utils.get(member.guild.text_channel, name = "welcome")
+    await channel.send(f"{member} is joined the discord channel.")
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You con't do that.")
+        await ctx.message.delete()
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please enter all the required arguments.")
+        await ctx.message.delete()
+    else:
+        raise error
+
+# @client.event
+# async def on_message(msg):
+#     for word in words:
+#         if word in msg.content:
+#             await msg.delete()
+
+#     await client.process_commands(msg)
+
+@client.command()    
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def curr(ctx, from_to_country: str = "", amount: int = 1):
+    embed = discord.Embed(title = "CURRENT CURRENCIES", color = discord.Colour.green())
+    embed.add_field(name="USD", value=current("USD", "TRY"))
+    embed.add_field(name="EUR", value=current("EUR", "TRY"))
+    if from_to_country != "" and amount != 1:
+        a,b = from_to_country.split("-")
+        a.upper(),b.upper()
+        if a == "USD":
+            embed.add_field(name="USD to TRY", value=convert(a, b, amount))
+        else: 
+            embed.add_field(name="EUR to TRY", value=convert(a, b, amount))
+    embed.set_footer(text="by Forex")
+    await ctx.send(embed=embed)
+
+@client.command()
+async def rand(ctx, x:int, y:int):
+    res = rando(x ,y)
+    await ctx.send(res)
+
+if __name__ == "__main__":
+    for filename in os.listdir("/home/abdullah/Desktop/Discord/Discord/cogs"):
+        if filename.endswith('.py'):
+            client.load_extension(f'cogs.{filename[:-3]}')
+            print(f"{filename} loaded")
+
+
+client.run(TOKEN)
---
+++ Discord/cogs/admin.py
@@ -0,0 +1,78 @@
+import discord
+from discord.ext import commands    
+
+class Mod(commands.Cog):
+    def __init__ (self, client):
+        self.client = client
+        self.activities = {}
+
+    @commands.command()
+    async def hello(self, ctx):
+        await ctx.send("hi")
+
+    @commands.command()
+    @commands.has_permissions(kick_members = True)
+    async def clear(self, ctx, line = 1):
+        await ctx.channel.purge(limit = line)
+        admin = ctx.author
+        print(f"{line} lines have been deleted by the {admin} .")
+   
+    @commands.command()
+    @commands.has_permissions(kick_members = True)
+    async def kick(self, ctx, member: discord.Member, *args, reason = "Pepega"):
+        await member.kick(reason = reason)
+        admin = ctx.author
+        print(f"{member} is kicked the channel by the {admin}.")
+
+    @commands.command()
+    @commands.has_permissions(ban_members = True)
+    async def ban(self, ctx, member: discord.Member, *args, reason = "Pepega"):
+        await member.ban(reason = reason)
+        admin = ctx.author
+        print(f"{member} is banned the channel by the {admin}.")
+
+    @commands.command()
+    @commands.has_permissions(ban_members = True)
+    async def unban(self, ctx, *, member):
+        banned_user = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for bans in banned_user:
+            user = bans.user
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f"Unbanned user {user.mention}")
+                return
+
+        admin = ctx.author
+        print(f"The ban of {member} user has been removed by the {admin}.")
+
+    @commands.command()
+    @commands.has_permissions(ban_members = True)
+    async def mute(self, ctx, member: discord.Member):
+        muted_role = ctx.guild.get_role(833793972731117639)
+        await member.add_roles(muted_role)
+        await ctx.send(member.mention + " has been muted.")
+        printf(f"{member} has been muted.")
+
+    @commands.command()
+    async def status(self, ctx, activity, *, state):
+        self.change_status(state)
+        await self.client.change_presence(**self.activities.get(activity))
+    # The python will execute according to the parameter length.
+    @commands.command()
+    async def status(self, ctx, activity, url, *, state):
+        self.change_status(state, url)
+        await self.client.change_presence(**self.activities.get(activity))
+
+    def change_status(self, state, url = ''):
+        self.activities = {
+            "game" : {"activity": discord.Game(name = state)},
+            "listening" : {"activity": discord.Activity(type = discord.ActivityType.listening, name = state)},
+            "watching" : {"activity": discord.Activity(type = discord.ActivityType.watching, name = state)},
+            "streaming" : {"activity": discord.Streaming(name = state, url = url)},
+        }
+
+# This functions is special for cogs. 
+def setup(client):
+    client.add_cog(Mod(client))
